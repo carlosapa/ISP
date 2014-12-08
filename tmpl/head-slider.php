d61c4c82bb403ce87aea02c1052c6b68b0fd5cc7
carlosapa <carlos_non86@msn.com>
2014-12-08 16:12:02 +0100

    Commit

--- tmpl/head-slider.php
+++ tmpl/head-slider.php
@@ -3,9 +3,14 @@
 		<div class="slider_back to-left w-10 h-all absolute"></div>
 		<div class="slider_holder w-1080 div-center h-all">
 			<ul class="slider_items">
-				<li class="slider_item"></li>
-				<li class="slider_item"></li>
-				<li class="slider_item"></li>
+				<li class="slider_item relative">
+					<div class="slider_item__block absolute w-40 h-all">
+						
+					</div>
+					<img src="img/rad_ph02.jpg" alt="rad_ph02" class="w-all" title="rad_ph02">
+				</li>
+				<li class="slider_item hidden"></li>
+				<li class="slider_item hidden"></li>
 			</ul>
 		</div>
 		<div class="slider_next to-left w-10 h-all absolute"></div>
