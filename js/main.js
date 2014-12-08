7b285d215d6895762758b6a5c1b3e278cfc05291
carlosapa <carlos_non86@msn.com>
2014-12-08 18:51:14 +0100

    20141208 #7 index_content commits

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+/*=== set equal the heights of the splitted content ===*/
 
+var set_heights = (function (win, doc, $) {
+	
+	var items = $('.splited_content__item');
+	var top_height = 0;
+
+	var getter = function () {
+		items.each(function (i, e) {
+			var height = $(e).height();
+			top_height = (height > top_height) ? height : top_height;
+		});		
+	};
+
+	var setter = function () {
+		items.each(function (i, e) {
+			$(e).css('height', top_height + 'px');
+		});
+	};
+
+	//onload
+	getter();
+	setter();
+
+	//onresize
+	$(win).on('resize', function () {
+		getter();
+		setter();		
+	})
+
+
+
+
+
+}(window, document, jQuery));
--- tmpl/index-content.php
+++ tmpl/index-content.php
@@ -64,5 +64,128 @@
 				</div>
 			</div>
 		</div>
-	</div>
+	</div> <!-- end of content__section services -->
+
+	<div class="content__section medium-space-before medium-space-after clearfix">
+		<h2 class="section_title">Wir über uns</h2>
+		<div class="splited_content clearfix">
+			
+			<div class="splited_content__item w-40 to-left bleed-right">
+				<div class="content_description medium-space-after">
+					<p>
+						Bauen mit Stahl ermöglicht wirtschaftliche und umweltfreundliche Lösungen 
+						Wir haben die Basis für die rasche und kompetente Ausführung verschiedenster Kundenwünsche:
+					</p>
+				</div>
+				<div class="content_image img_holder-all">
+					<div class="content_image__holder">
+						<img src="img/rad_ph02.jpg" alt="" class="w-all">
+					</div>
+					<div class="caption_offset">
+						<span>Picture Caption 1</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="splited_content__item w-60 to-right bleed-left bg_grey">
+				<ul class="content_list bleed-inside">
+					<li class="content_list__items">ISP ist 27 Jahre im Segment Stahlbauplanung tätig</li>
+					<li class="content_list__items">20 qualifizierte Mitarbeiter und Mitarbeiterinnen</li>
+					<li class="content_list__items">27 Jahre Erfahrung eines modernen Büros ein umfangreiches Leistungssortiment</li>
+					<li class="content_list__items">Die technische Ausstattung mit 20 CAD Arbeitsplätzen und zugehöriger </li>
+					<li class="content_list__items">Software für Statik, Planung und Konstruktion</li>
+					<li class="content_list__items">Mitgliedschaften zum Vorteil unserer Kunden</li>
+					<li class="content_list__items">Beratende Ingenieure IK Bau NW</li>
+					<li class="content_list__items">Deutscher Stahlbauverband</li>
+				</ul>
+			</div>
+
+		</div>
+	</div> <!-- end of content__section services -->
+
+	<div class="content__section medium-space-before medium-space-after clearfix">
+		<h2 class="section_title">Unsere Stärken</h2>
+		<div class="splited_content clearfix">
+			
+			<div class="splited_content__item w-40 to-left bleed-right">
+				<div class="content_description medium-space-after">
+					<p>
+						Kompetenz und Innovation zu Ihrem Vorteil: 
+						Folgerichtige und konsequente Schritte zu zeitgemäßen Stahlkonstruktionen.
+					</p>
+				</div>
+				<div class="content_image img_holder-all">
+					<div class="content_image__holder">
+						<img src="img/rad_ph02.jpg" alt="" class="w-all">
+					</div>
+					<div class="caption_offset">
+						<span>Picture Caption 1</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="splited_content__item w-60 to-right bleed-left bg_grey">
+				<ul class="content_list bleed-inside">
+					<p class="content_list__items">
+						Wir planen ihre Stahlkonstruktionen für: 
+						Chemieanlagen, Hüttenwerke, Kraftwerke, Zementwerke, Förderanlagen, Kokereien, Umwelttechnik, Sonderkonstruktionen, Versammlungsbauten, Sportstätten, Hallenbau.
+						Leistungen für den Industrie- und Hochbau:
+					</p>
+
+					<li class="content_list__items">Beratung bei Projektgrundlagenerstellung</li>
+					<li class="content_list__items">Eigenverantwortliche Problemlösungen bei Projekterstellung</li>
+					<li class="content_list__items">Problemlösung von Bauherrenaufgaben</li>
+					<li class="content_list__items">Problemlösung von Objektplanungsaufgaben</li>
+					<li class="content_list__items">Problemlösung von Anlagenplanungsaufgaben</li>
+					<li class="content_list__items">Berücksichtigung von Fertigungsvorgaben</li>
+					<li class="content_list__items">Baustellenüberwachung</li>
+				</ul>
+			</div>
+
+		</div>
+	</div> <!-- end of content__section services -->
+
+	<div class="content__section medium-space-before medium-space-after clearfix">
+		<h2 class="section_title">Unsere Stärken</h2>
+		<div class="splited_content clearfix">
+			
+			<div class="splited_content__item w-40 to-left bleed-right">
+				<div class="content_description medium-space-after">
+					<p>
+						Kompetenz und Innovation zu Ihrem Vorteil: 
+						Folgerichtige und konsequente Schritte zu zeitgemäßen Stahlkonstruktionen.
+					</p>
+				</div>
+				<div class="content_image img_holder-all">
+					<div class="content_image__holder">
+						<img src="img/rad_ph02.jpg" alt="" class="w-all">
+					</div>
+					<div class="caption_offset">
+						<span>Picture Caption 1</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="splited_content__item w-60 to-right bleed-left bg_grey">
+				<ul class="content_list bleed-inside">
+					<p class="content_list__items">
+						Wir planen ihre Stahlkonstruktionen für: 
+						Chemieanlagen, Hüttenwerke, Kraftwerke, Zementwerke, Förderanlagen, Kokereien, Umwelttechnik, Sonderkonstruktionen, Versammlungsbauten, Sportstätten, Hallenbau.
+						Leistungen für den Industrie- und Hochbau:
+					</p>
+					
+					<li class="content_list__items">Beratung bei Projektgrundlagenerstellung</li>
+					<li class="content_list__items">Eigenverantwortliche Problemlösungen bei Projekterstellung</li>
+					<li class="content_list__items">Problemlösung von Bauherrenaufgaben</li>
+					<li class="content_list__items">Problemlösung von Objektplanungsaufgaben</li>
+					<li class="content_list__items">Problemlösung von Anlagenplanungsaufgaben</li>
+					<li class="content_list__items">Berücksichtigung von Fertigungsvorgaben</li>
+					<li class="content_list__items">Baustellenüberwachung</li>
+				</ul>
+			</div>
+
+		</div>
+	</div> <!-- end of content__section services -->
+
+
 </div>
