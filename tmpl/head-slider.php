9bfcd2bae66bd8a85648ca3e4c589d07c2b4179d
carlosapa <carlos_non86@msn.com>
2014-12-08 18:04:48 +0100

    20141208 #6 Index Content

--- tmpl/head-slider.php
+++ tmpl/head-slider.php
@@ -1,11 +1,23 @@
 <div class="head_slider">
 	<div class="head_slider__holder h-all relative clearfix">
-		<div class="slider_back to-left w-10 h-all absolute"></div>
+		<div class="slider_back to-left w-10 h-all absolute">
+			<img src="img/arrows_left.png" title="navigate to left" alt="to_left" class="clickable"/>
+		</div>
+		
 		<div class="slider_holder w-1080 div-center h-all">
 			<ul class="slider_items">
 				<li class="slider_item relative">
 					<div class="slider_item__block absolute w-40 h-all">
-						
+						<h1>Willkommen bei ISP</h1>
+						<hr>
+						<p>
+							Ihrem Partner für Planung, Statik und Konstruktion von Stahlbauten und Industrieanlagen.
+							Diese Präsentation soll Ihnen einen Überblick über unsere Leistungsfähigkeit bieten.
+						</p>
+						<p>
+							Weitere Informationen geben wir Ihnen gerne. Bitte nehmen Sie Kontakt auf -
+							telefonisch, per E-mail oder über unser Feedback- Formular.
+						</p>
 					</div>
 					<img src="img/rad_ph02.jpg" alt="rad_ph02" class="w-all" title="rad_ph02">
 				</li>
@@ -13,6 +25,9 @@
 				<li class="slider_item hidden"></li>
 			</ul>
 		</div>
-		<div class="slider_next to-left w-10 h-all absolute"></div>
+		
+		<div class="slider_next to-left w-10 h-all absolute talign_right">
+			<img src="img/arrows_right.png" title="navigate to right" alt="to_right" class="clickable"/>
+		</div>
 	</div>
 </div>
---
+++ tmpl/index-content.php
@@ -0,0 +1,68 @@
+<div class="content w-1080 div-center clearfix">
+	
+	<div class="content__section medium-space-before medium-space-after clearfix">
+		<h2 class="section_title">Dienstleistungen</h2>
+		<div class="grid-6 clearfix">
+			<div class="service subgrid-6 clearfix">
+				<div class="services relative">
+					<div class="services_image w-all h-all">
+						<img src="img/foto.jpg" alt="" class="w-all">
+					</div>
+					<div class="services_title absolute w-all">
+						Tragwerks-Plannung
+					</div>
+				</div>
+			</div>
+			<div class="service subgrid-6 clearfix">
+				<div class="services relative">
+					<div class="services_image w-all h-all">
+						<img src="img/foto-1.jpg" alt="" class="w-all">
+					</div>
+					<div class="services_title absolute w-all">
+						Statik
+					</div>
+				</div>
+			</div>
+			<div class="service subgrid-6 clearfix">
+				<div class="services relative">
+					<div class="services_image w-all h-all">
+						<img src="img/rad_ph02.jpg" alt="" class="w-all">
+					</div>
+					<div class="services_title absolute w-all">
+						Konstruktion
+					</div>
+				</div>
+			</div>
+			<div class="service subgrid-6 clearfix">
+				<div class="services relative">
+					<div class="services_image w-all h-all">
+						<img src="img/dachtraeger3.jpg" alt="" class="w-all">
+					</div>
+					<div class="services_title absolute w-all">
+						Machbarkeitsstudien
+					</div>
+				</div>
+			</div>
+			<div class="service subgrid-6 clearfix">
+				<div class="services relative">
+					<div class="services_image w-all h-all">
+						<img src="img/dachtraeger2.jpg" alt="" class="w-all">
+					</div>
+					<div class="services_title absolute w-all">
+						Bauen im Bestand
+					</div>
+				</div>
+			</div>
+			<div class="service subgrid-6 clearfix">
+				<div class="services relative">
+					<div class="services_image w-all h-all">
+						<img src="img/dachkonstruktion.jpg" alt="" class="w-all">
+					</div>
+					<div class="services_title absolute w-all">
+						Objekt Plannung
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
