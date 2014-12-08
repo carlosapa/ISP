fbd53c7e6c24f139b11b13270b19550857b4e2ae
carlosapa <carlos_non86@msn.com>
2014-12-08 13:37:51 +0100

    20140108 #5

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once('tmpl/header.php'); ?>
+
+<?php include_once('tmpl/head-main.php'); ?>
+<?php include_once('tmpl/head-slider.php'); ?>
+<?php include_once('tmpl/index-content.php'); ?>
+
+<?php include_once('tmpl/footer-map.php'); ?>
+
+<?php include_once('tmpl/footer.php'); ?>
+
---
+++ tmpl/head-main.php
@@ -0,0 +1,60 @@
+<div class="head">
+	<div class="head_line w-all"></div>
+
+	<div class="head__holder w-1080 div-center micro-space-before">
+		
+		<div class="head_logo to-left w-25 ">
+			<div class="head_logo__holder w-all">
+				<img src="img/ISP-Logo neu positiv blau.png" alt="logo" title="logo" class="w-40">
+			</div>
+		</div> <!-- end of head logo -->
+		
+		<div class="head_navigation to-right w-75 talign_right">
+			<div class="navigation__holder">
+				<ul class="navigation_items">
+					<li class="navigation_item navigation_menu">
+						<a href="#" target="_blank" class="clean_link">
+							<span>Unternehmen</span>
+						</a>
+						</li>
+					<li class="navigation_item navigation_menu">
+						<a href="#" target="_blank" class="clean_link">
+							<span>Dienstleistungen</span>
+						</a>
+						</li>
+					<li class="navigation_item navigation_menu">
+						<a href="#" target="_blank" class="clean_link">
+							<span>Projekte</span>
+						</a>
+						</li>
+					<li class="navigation_item navigation_menu">
+						<a href="#" target="_blank" class="clean_link">
+							<span>Karriere</span>
+						</a>
+						</li>
+					<li class="navigation_item navigation_menu">
+						<a href="#" target="_blank" class="clean_link">
+							<span>Kontakt</span>
+						</a>
+						</li>
+					<li class="navigation_item navigation_flags">
+						<a href="#" target="_blank">
+							<img src="img/lang-menu-de.png" alt="navigation_flag" title="DE">
+						</a>
+					</li>
+					<li class="navigation_item navigation_flags">
+						<a href="#" target="_blank">
+							<img src="img/lang-menu-es.png" alt="navigation_flag_ES" title="ES">
+						</a>
+					</li>
+					<li class="navigation_item navigation_flags">
+						<a href="#" target="_blank">
+							<img src="img/lang-menu-en.png" alt="navigation_flag_EN" title="EN">
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div> <!-- end of head navigation -->
+	
+	</div> <!-- end of head holder -->
+</div> <!-- end of head  -->
