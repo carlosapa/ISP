a447eb3c6d34502e8cf967ad0848adb0674c4ac0
carlosapa <carlos_non86@msn.com>
2014-12-08 15:32:28 +0100

    Commit

---
+++ tmpl/head-slider.php
@@ -0,0 +1,13 @@
+<div class="head_slider">
+	<div class="head_slider__holder h-all relative clearfix">
+		<div class="slider_back to-left w-10 h-all absolute"></div>
+		<div class="slider_holder w-1080 div-center h-all">
+			<ul class="slider_items">
+				<li class="slider_item"></li>
+				<li class="slider_item"></li>
+				<li class="slider_item"></li>
+			</ul>
+		</div>
+		<div class="slider_next to-left w-10 h-all absolute"></div>
+	</div>
+</div>
