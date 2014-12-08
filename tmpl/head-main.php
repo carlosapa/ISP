e6aaf5ba53c138f9dd74c0faccc857ad074a7fb4
carlosapa <carlos_non86@msn.com>
2014-12-08 14:10:05 +0100

    Mod. Menu

--- tmpl/head-main.php
+++ tmpl/head-main.php
@@ -1,7 +1,7 @@
 <div class="head">
 	<div class="head_line w-all"></div>
 
-	<div class="head__holder w-1080 div-center micro-space-before">
+	<div class="head__holder w-1080 div-center micro-space-before clearfix">
 		
 		<div class="head_logo to-left w-25 ">
 			<div class="head_logo__holder w-all">
@@ -9,9 +9,9 @@
 			</div>
 		</div> <!-- end of head logo -->
 		
-		<div class="head_navigation to-right w-75 talign_right">
-			<div class="navigation__holder">
-				<ul class="navigation_items">
+		<div class="head_navigation to-right w-75">
+			<div class="navigation__holder clearfix ">
+				<ul class="navigation_items menu-inline clearfix talign_right">
 					<li class="navigation_item navigation_menu">
 						<a href="#" target="_blank" class="clean_link">
 							<span>Unternehmen</span>
