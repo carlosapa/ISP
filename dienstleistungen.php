8b582f05414ca5d5f0acc5231d17dfa2ec030b23
diegoruizdelarbol <diegoruizdelarbol@gmail.com>
2014-12-12 13:30:45 +0100

    Cambios en contenido
    Diego: Cambios en contenido para presentar a Alex

--- dienstleistungen.php
+++ dienstleistungen.php
@@ -78,9 +78,8 @@ $srcs = array(
 							<li class="content_list__items">Software für Statik, Planung und Konstruktion</li>
 							<li class="content_list__items">Mitgliedschaften zum Vorteil unserer Kunden</li>
 						</ul>
-						<h3>Historie</h3>
+						<h3>Voluptatem sequi nesciunt</h3>
 						<p class="talign_justify">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
-						<p class="talign_justify">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
 						<p class="talign_justify">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. </p>
 					</div>
 					<?php endif; ?>
